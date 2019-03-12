b7ae2fa6270857f9faba8a314f915334d8d270fe
imnotmaddy <mixasok@bk.ru>
2019-03-12 10:29:20 +0300

    Created Entity class for future DB classes Created Contact class containing all fields.

---
+++ src/main/java/app/models/Contact.java
@@ -0,0 +1,39 @@
+package app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+//TODO rename Contact->User ?
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Contact extends Entity {
+    @NonNull private String telephoneNumber;
+
+    //TODO mobile phone number or stationary phone?
+
+    private String email;
+    @NonNull private String name;
+    @NonNull private String surname;
+    private String familyName;
+    private LocalDateTime dateOfBirth;
+
+    //TODO gender? boolean or enum or string or int?
+
+    private String citizenship;
+    private String relationship;
+    private String webSite;
+    private String currentJob;
+    private String jobAdress;
+    private String residenceCountry;
+    private String residenceCity;
+    private String street;
+    private int residenceHouseNumber;
+    private int residenceApartmentNumber;
+    private int index;
+
+
+}
---
+++ src/main/java/app/models/Entity.java
@@ -0,0 +1,8 @@
+package app.models;
+
+import lombok.Data;
+
+@Data
+public abstract class Entity {
+    private int id;
+}
